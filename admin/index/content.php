0b4847c9055efb53e939556be11fa105aa1b0217
Yonathan Castillo <leothan522@gmail.com>
2023-09-20 07:10:22 -0400

    Integranción de phpspreadsheet para generar Excel

--- admin/index/content.php
+++ admin/index/content.php
@@ -1,5 +1,6 @@
 <div class="row">
     <div class="col-12">
-        Bienvenido <?php echo $controller->USER_NAME ?>.
+        Bienvenido <?php echo $controller->USER_NAME ?>. <br>
+        <a href="_component/export_excel.php">Ejemplo de Export Excel.</a>
     </div>
 </div>
